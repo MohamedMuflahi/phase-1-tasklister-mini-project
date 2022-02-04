12d8e465e6d1c05c88b59df9e961836be138df2e
mohamed muflahi <muflahimohamed88@gmail.com>
2022-02-04 13:53:31 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const textBox = document.getElementById("new-task-description");
+  const ul = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", logsubmit);
+  
+  function logsubmit(event){
+    event.preventDefault();
+    submitButton();
+  }
+  function submitButton(){
+    let li = document.createElement("LI");
+    li.innerHTML = textBox.value;
+    let button = document.createElement("button");
+    button.innerHTML = "x";
+    button.addEventListener('click',() =>{
+      li.remove();
+    });
+    li.appendChild(button)
+    ul.appendChild(li);
+  }
 });
+
